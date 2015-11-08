9ca1956ca63909d9c4d7d3b319a007a125c43643
Frank Pham <pnanh@me.com>
2015-11-08 13:20:31 +0700

    version 1.0

--- OCRDemo/ViewController.m
+++ OCRDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <TesseractOCR/TesseractOCR.h>
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+- (IBAction)GetImageAction:(id)sender;
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *_Indicator;
 
 @end
 
@@ -26,4 +30,50 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)GetImageAction:(id)sender {
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    imagePicker.editing = true;
+    imagePicker.sourceType =  UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:imagePicker animated:true completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    self.imgView.image = image;
+    
+    [picker dismissViewControllerAnimated:true completion:nil];
+    self._Indicator.hidden = false;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+    
+        
+        G8Tesseract *analyzer = [[G8Tesseract alloc] init];
+        analyzer.language = @"eng";
+        analyzer.engineMode = G8OCREngineModeTesseractCubeCombined;
+        analyzer.pageSegmentationMode =G8PageSegmentationModeAuto;
+        analyzer.maximumRecognitionTime = 60.0;
+        analyzer.image = image.g8_blackAndWhite;
+        [analyzer recognize];
+        
+        dispatch_async(dispatch_get_main_queue(), ^(void){
+        
+            self._Indicator.hidden = true;
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Message" message:[analyzer recognizedText] preferredStyle:UIAlertControllerStyleAlert];
+            id OK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+            [alert addAction:OK];
+            [self presentViewController:alert animated:true completion:nil];
+        
+        });
+    
+    
+    });
+    
+    
+    
+    
+    
+}
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:true completion:nil];
+}
 @end
